ca7b883443d231217eb98f9bd2838fb3e51aae6a
Sswastik60 <sswastik60@gmail.com>
2026-01-14 19:17:08 +0530

    Added the warmth feature

--- lab1/Game.java
+++ lab1/Game.java
@@ -1,17 +1,21 @@
 package Java_practice.OOPS.lab1;
 import java.util.Scanner;
 
-public class Player {
+class Player {
     String name;
     int health=100;
     int level=1;
     int hunger=10;//initial energy increases as the player eats something
     int stamina=100;//decrease if you attack someone
+    int warmth=50;
 
     void displayStatus(){
         System.out.println("Name:"+name);
         System.out.println("Health:"+health);
         System.out.println("Level:"+level);
+        System.out.println("Hunger:"+hunger);
+        System.out.println("Stamina:"+stamina);
+        System.out.println("Warmth:"+warmth);
     }
 
 }
@@ -20,7 +24,7 @@ class Firewood {
     int warmth =20;
     void use(Player p){
         System.out.println("Player used Firewood!");
-        p.health=p.health + warmth;
+        p.warmth=p.warmth + warmth;
     }
    
 }
@@ -42,7 +46,18 @@ public class Game {
 
         System.out.println("You feel weak,The room is cold.\nYour body remembers hunger… and thirst.\nSomething lies nearby.");
 
-        System.out.println("You see a firewood lying nearby!");
-        warmth warmth1 = new Firewood();
+        System.out.println("You see a firewood lying nearby! \nUsing a firewood would increase warmth by +20");
+        Firewood warmth1 = new Firewood();
+        System.out.println("Do you want to use firewood 1=yes ;0=no?");
+        int in =sc.nextInt();
+        if (in == 1) {
+            warmth1.use(p1);
+            p1.displayStatus();
+            // System.out.println("inside if");
+        }
+
+
+
+           
     }
 }
