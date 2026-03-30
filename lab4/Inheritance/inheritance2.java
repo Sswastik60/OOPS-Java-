210862503ce27f456ed2600e60a9f8fb6705af53
Sswastik60 <sswastik60@gmail.com>
2026-03-30 13:29:58 +0530

    Inheritance v 3.0

--- lab4/Inheritance/inheritance2.java
+++ lab4/Inheritance/inheritance2.java
@@ -1,15 +1,39 @@
 package Inheritance;
+//multiple inheritance not allowed in java
 abstract class character{//now we cannot create objects of this abstract class.
     String name;
+    // abstract void attack1(){
+    //     System.out.println("This ensures there should be a attack1 method in every child class");
+    // }WE CANT CREATE AN OBJECT OF character AND THERE SHOULD BE attack1 method in each child class ponly abstract classes can have abstract methods because if abstract class is not there we might create a character object and try to run the abstract method which is not possible.
     void attack(character c){
         System.out.println("Attacked");
     }
     
 }
+
+//INTERFACE we can implement n number of interfaces but there is only one class that can extended.
+
+interface flyable{
+    int max_ht=100000; //final keyword it becomes a constant and we cant change the value (will be public, static and final)
+
+    default void fly(){
+
+    }
+    default void land(){
+
+    }
+
+    //to write code inside the interface we have to use the keyword default
+    default void fly1(){
+
+    }
+}
 //child can call father not grandfather branch to access grand father from the child class we have to call it step wise
 
-class mage extends character{
+class mage extends character implements flyable{//the interface methods should be in this class
     int magicpower;
+    
+
     String n = super.name;//to access data from the super class similarily any methods or functions can also be called including the static variables also using the super.() 
     @Override // (not mandatory but used to make the code more reliable)it doesnt matter even if we dont use it bbut if we still write it to make sure the function operates correctly
     void attack(character c){
@@ -24,6 +48,7 @@ class mage extends character{
 
 class knight extends character{
     int strength;
+
     void attack(character c){
         System.out.println("Thunder bolt");
     }
@@ -31,6 +56,7 @@ class knight extends character{
 
 public class inheritance2 {
     public static void main(String[] args) {
+        // character c = new character();this is not possible if the class is an abstract class
         character hero= new knight();
         character villain = new mage();
         hero.attack(villain);
